f0dd644a68a6e057bf14c859963b048e89f2a3d6
basarat <basaratali@gmail.com>
2022-06-05 14:03:49 +1000

    cleaner

--- src/Login.tsx
+++ src/Login.tsx
@@ -12,18 +12,18 @@ export const Login: React.FC<{}> = () => {
 
   return (<>
       <div className='mb-3'>
-        <label htmlFor='exampleInputEmail' className='form-label'>Email address</label>
-        <input type='email' className='form-control' id='exampleInputEmail' aria-describedby='emailHelp' />
+        <label htmlFor='inputEmail' className='form-label'>Email address</label>
+        <input type='email' className='form-control' id='inputEmail' aria-describedby='emailHelp' />
       </div>
       <div className='mb-3'>
-        <label htmlFor='exampleInputPassword' className='form-label'>Password</label>
-        <input type='password' className='form-control' id='exampleInputPassword' />
+        <label htmlFor='inputPassword' className='form-label'>Password</label>
+        <input type='password' className='form-control' id='inputPassword' />
       </div>
       <div className='mb-3 form-check'>
         <input type='checkbox' className='form-check-input' id='rememberMe' />
         <label className='form-check-label' htmlFor='rememberMe'>Remember Me</label>
       </div>
-      <Button type='button' className='btn btn-primary' onClick={handleSubmit}>Submit</Button>
+      <Button type='button' id='submit' className='btn btn-primary' onClick={handleSubmit}>Submit</Button>
   </>);
 }
 
